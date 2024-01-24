43fd3581b5c12de37cade4b1f9752d52a0a7a38b
DecidedCard <heajun35@gmail.com>
2024-01-24 10:21:39 +0900

    feat: 기본 컴포넌트 분리

---
+++ src/components/Layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Main from "./main/Main";
+
+function Layout() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
+
+export default Layout;
